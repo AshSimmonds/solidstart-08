d93f70762bafe5faaca67ddd8e2b6ee063d9c440
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-28 22:08:43 +1030

    xcom oneday

---
+++ src/routes/xcom.tsx
@@ -0,0 +1,19 @@
+import Layout from "~/components/Layout"
+import PageTitle from "~/components/PageTitle"
+
+export default function XComCentralPage() {
+    return (
+        <Layout>
+            <PageTitle>Central</PageTitle>
+            <h1>Central</h1>
+
+            <div class="text-center mx-auto ">
+
+                <div>
+                    XState POC: https://codesandbox.io/s/xstate-solid-example-dgpd7?file=/index.jsx
+                </div>
+
+            </div>
+        </Layout>
+    )
+}
