915d7e71ead987d8b62eded8da233b63dc24bc88
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-30 08:23:36 +1030

    zag toggle works, noice

--- src/routes/zag.tsx
+++ src/routes/zag.tsx
@@ -1,19 +1,29 @@
 import Layout from "~/components/Layout"
 import PageTitle from "~/components/PageTitle"
+import { createMemo } from "solid-js"
+import * as toggle from "@zag-js/toggle"
+import { useMachine, normalizeProps } from "@zag-js/solid"
 
 export default function ZagPage() {
+    const [state, send] = useMachine(toggle.machine({
+        id: "1"
+    }))
+
+    const api = createMemo(() => toggle.connect(state, send, normalizeProps))
+
     return (
         <Layout>
-            <PageTitle>zag</PageTitle>
+            <PageTitle>zag | zagjs.com</PageTitle>
             <h1>zag</h1>
+
+            <h2>https://zagjs.com</h2>
 
             <div class="text-center mx-auto ">
 
-                <h2>more stuff stuff</h2>
-
-                <div>
-                    qwer
-                </div>
+                <h3>toggle</h3>
+                <button {...api().buttonProps}>
+                    {api().isPressed ? "On" : "Off"}
+                </button>
 
             </div>
         </Layout>
