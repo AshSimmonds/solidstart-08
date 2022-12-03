ed6198f4736ddf262728527186ffe31defdeb0db
Ash Simmonds <ashsimmonds@gmail.com>
2022-12-03 18:48:36 +1030

    enough AoC for now

--- src/routes/aoc2203.tsx
+++ src/routes/aoc2203.tsx
@@ -20,16 +20,25 @@ export const routeData = () => {
             const rucksacksOne: any[] = []
             const rucksacksTwo: any[] = []
 
-            let totalPriorityTest = 0
+            const shortages: any[] = []
 
-            theDataTest.split("\r\n").forEach((line) => {
+            let totalPriorityTest = 0
+            let totalPriorityOne = 0
+            let totalPriorityTwo = 0
+
+            let lineCownt = 0
+
+            let shortage: any = []
+
+
+            theDataOne.split("\r\n").forEach((line) => {
 
                 const compartmentOne = line.substring(0, line.length / 2)
                 const compartmentTwo = line.substring(line.length / 2, line.length)
 
                 const itemsInBothCompartments = compartmentOne.split("").filter((item) => compartmentTwo.includes(item))[0]
 
-                
+
                 let priority = 0
 
                 if (itemsInBothCompartments === itemsInBothCompartments.toLowerCase()) {
@@ -38,20 +47,52 @@ export const routeData = () => {
                     priority = itemsInBothCompartments.charCodeAt(0) - 38
                 }
 
-                totalPriorityTest += priority
+                // totalPriorityTest += priority
+                totalPriorityOne += priority
 
-                rucksacksTest.push({
+                rucksacksOne.push({
                     compartmentOne,
                     compartmentTwo,
                     itemsInBothCompartments,
                     priority
                 })
 
+
+
+
+
+                shortage.push(line)
+
+                let stickerPriority = 0
+
+                if (lineCownt === 2) {
+
+                    const itemInAllThree = shortage[0].split("").filter((item: any) => shortage[1].includes(item) && shortage[2].includes(item))[0]
+
+                    console.log(`shortage: ${shortage}`)
+
+                    if (itemInAllThree === itemInAllThree.toLowerCase()) {
+                        stickerPriority = itemInAllThree.charCodeAt(0) - 96
+                    } else {
+                        stickerPriority = itemInAllThree.charCodeAt(0) - 38
+                    }
+
+                    totalPriorityTwo += stickerPriority
+
+                    lineCownt = 0
+                    shortage = []
+                } else {
+
+                    lineCownt++
+                }
+
             })
 
             const theResultTest = totalPriorityTest
+            const theResultOne = totalPriorityOne
+            const theResultTwo = totalPriorityTwo
 
-            return { testData: theDataTest, testRucksacks: rucksacksTest, testResult: theResultTest, dataOne: theDataOne, dataTwo: theDataTwo }
+            return { testData: theDataTest, testRucksacks: rucksacksTest, testResult: theResultTest, oneData: theDataOne, oneRucksacks: rucksacksOne, oneResult: theResultOne, twoResult: theResultTwo }
 
         } catch (error) {
             console.error(`qwer: ${error}`)
@@ -105,7 +146,7 @@ export default function AdventOfCodePage() {
                 </Show>
             </pre>
 
-            <h4>RESULT</h4>
+            <h3>RESULT</h3>
 
             <div class="alert alert-success">
                 <Show when={theLore()?.testRucksacks}>
@@ -116,6 +157,14 @@ export default function AdventOfCodePage() {
                 Full payload: <a href="/aoc/aoc2203.txt">aoc2203.txt</a>
             </p>
 
+
+            <h2>Puzzle One</h2>
+            <h4>RESULT</h4>
+            <div class="alert alert-success">{theLore()?.oneResult}</div>
+
+            <h2>Puzzle Two</h2>
+            <h4>RESULT</h4>
+            <div class="alert alert-success">{theLore()?.twoResult}</div>
 
 
             <h2>Full data</h2>
