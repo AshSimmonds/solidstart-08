266f8c49653f42e11ef5c19007514fadf74df1e9
Ash Simmonds <ashsimmonds@gmail.com>
2022-12-05 01:24:55 +1030

    scaffold out space cock

---
+++ src/routes/saber/spacecock.tsx
@@ -0,0 +1,16 @@
+import Layout from "~/components/Layout"
+import PageTitle from "~/components/PageTitle"
+
+export default function SpaceCockPage() {
+    return (
+        <Layout>
+            <PageTitle>Space Cockpit | Saber Astronautics</PageTitle>
+            <h1>Space Cockpit | Saber Astronautics</h1>
+
+            <div class="text-center mx-auto ">
+
+
+            </div>
+        </Layout>
+    )
+}
