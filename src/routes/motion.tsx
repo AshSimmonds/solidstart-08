b226b8e4302d7a72255f3f8e02bb3654dd563876
Ash Simmonds <ashsimmonds@gmail.com>
2022-12-04 11:44:31 +1030

    click to blue

--- src/routes/motion.tsx
+++ src/routes/motion.tsx
@@ -1,21 +1,24 @@
 import Layout from "~/components/Layout"
 import PageTitle from "~/components/PageTitle"
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import { Motion } from "@motionone/solid";
 
 export default function MotionPage() {
+
     return (
         <Layout>
             <PageTitle>Motion One / Dev</PageTitle>
             <h1>Motion One / Dev</h1>
 
-            <div class="text-center mx-auto ">
-                <MotionDefault />
+            <MotionDefault />
 
-                <MotionRotate90 />
+            <MotionRotate90 />
 
-                <MotionRotate90Slow />
-            </div>
+            <MotionRotate90Slow />
+
+            <MotionClickToChangeBackground />
+
+
         </Layout>
     )
 }
@@ -63,5 +66,24 @@ const MotionRotate90Slow: Component = () => {
             transition=duration: 1,
             rotate: duration: 4
         </Motion.div>
+    )
+}
+
+
+const MotionClickToChangeBackground: Component = () => {
+    const [bg, setBg] = createSignal("red")
+
+    return (
+        <Motion.button
+            onClick={() => setBg("blue")}
+            animate={{
+                backgroundColor: bg(),
+            }}
+            transition={{
+                duration: 3,
+            }}
+        >
+            onClick=setBg("blue")
+        </Motion.button>
     )
 }
