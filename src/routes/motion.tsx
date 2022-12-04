11ff5e720da20fb360383dc9172a08e22a2057b8
Ash Simmonds <ashsimmonds@gmail.com>
2022-12-04 12:29:34 +1030

    svg path drawing, does not work

--- src/routes/motion.tsx
+++ src/routes/motion.tsx
@@ -35,6 +35,9 @@ export default function MotionPage() {
 
             <h2>SVG loading spinner - <code>OFFSET</code> unknown</h2>
             <MotionSVGLoadingSpinner offset={0} segments={8} />
+
+            <h2>SVG path drawing (✓ in a circle)</h2>
+            <MotionSVGPathDrawing />
         </Layout>
     )
 }
@@ -202,6 +205,40 @@ const MotionSVGLoadingSpinner: Component<{ offset: number, segments: number }> =
                     </g>
                 )}
             </Repeat>
+        </svg>
+    )
+}
+
+
+
+const MotionSVGPathDrawing: Component = (props) => {
+    const draw = (progress: number) => ({
+        // This property makes the line "draw" in when animated
+        strokeDashoffset: 1 - progress,
+
+        // Each line will be hidden until it starts drawing
+        // to fix a bug in Safari where the line can be
+        // partially visible even when progress is at 0
+        visibility: "visible",
+    })
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+            <Motion.circle
+                cx="100"
+                cy="100"
+                r="80"
+                pathLength="1"
+                animate={draw(1)}
+                transition={{ duration: 0.8, delay: 2 }}
+                class="bg-secondary p-4"
+            />
+            <Motion.path
+                d="M 54 107.5 L 88 138.5 L 144.5 67.5"
+                pathLength="1"
+                animate={draw(1)}
+                transition={{ duration: 0.6, delay: 2.4 }}
+            />
         </svg>
     )
 }
