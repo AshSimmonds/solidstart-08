bd54f4fe8c7bf271bd63d565185b12a1e23e84d8
Ash Simmonds <ashsimmonds@gmail.com>
2022-12-06 01:29:51 +1030

    fick refs

--- src/routes/saber/spacecock.tsx
+++ src/routes/saber/spacecock.tsx
@@ -4,6 +4,8 @@ import { createServerData$ } from "solid-start/server"
 import Layout from "~/components/Layout"
 import PageTitle from "~/components/PageTitle"
 
+import { Convert, SpaceCock } from "../../types/spacecock"
+
 const theSourceFile = "saber/space-cock_custom-scene_test-001.sc"
 const theSourceHost = "http://localhost:3000/"
 const theSource = theSourceHost + theSourceFile
@@ -19,7 +21,12 @@ export const routeData = () => {
 
             console.log(`saber spacecock routeData theData: ${theData}`)
 
+            // TODO: figure out why this errors out, error object is empty
+            // without this "theData" object does not have types properly specced
+            // https://app.quicktype.io/
+            // const spaceCock = Convert.toSpaceCock(theData)
 
+            // console.log(`saber spacecock routeData spaceCock: ${spaceCock}`)
 
             return theData
 
@@ -68,15 +75,15 @@ export default function SpaceCockPage() {
 
                         <h4>Active asset list index: {theLore()?.ActiveAssetListIndex}</h4>
 
-                        <h4>Focused Satellite: {theLore()?.FocusedSensor}</h4>
+                        <h4>Focused Satellite: {theLore()?.FocusedSat}</h4>
 
-                        <h4>Focused Sensor: {theLore()?.ActiveSat}</h4>
+                        <h4>Focused Sensor: {theLore()?.FocusedSensor}</h4>
 
                         <h4>Asset Lists</h4>
                         <For each={theLore()?.AssetLists}>
                             {(theAssetList) => (
                                 <div class="">
-                                    <h5>{theAssetList?.Name}</h5>
+                                    <h5>{theAssetList.Name}</h5>
                                     <h6>Satellites</h6>
                                     <div class="grid grid-cols-4">
                                         <For each={theAssetList?.Sats}>
@@ -94,7 +101,7 @@ export default function SpaceCockPage() {
                                         </For>
                                     </div>
 
-                                    <h6>Sensors</h6>
+                                    <h6>Ground stations</h6>
                                     <div class="grid grid-cols-4">
                                         <For each={theAssetList?.Sensors}>
                                             {(theSensor) => (
@@ -164,6 +171,24 @@ export default function SpaceCockPage() {
                     </div>
                 </div>
             </section >
+
+
+            <h2 >Bugs</h2>
+            <section id="xcomgreenright" data-augmented-ui="" class="mt-8 pt-1 p-12">
+                <ul>
+                    <li>invalid UDL credentials</li>
+                    <li>focused cam controls - roll should be Q and E, zoom should be R and F</li>
+                    <li>free cam controls - most keys wonky, right button is not for drag</li>
+                    <li>ground station search filter text invisible</li>
+                    <li>state machine of sat visibility a bit janky</li>
+                    <li>sensor tasking (tracking overpasses) - cannot deselect overpass</li>
+                    <li>show sat stuff like altitude/speed sometimes works sometimes not</li>
+                </ul>
+
+
+            </section>
+
+
 
             <h2>Source data</h2>
             <A href={theSource}>{theSource}</A>
